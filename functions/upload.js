00e521cef7c2aa469feead1f2c6c4463fac318d2
scavin <scavina@gmail.com>
2025-03-04 10:22:22 +0800

    修复文件名带空格时返回链接bug问题

--- functions/upload.js
+++ functions/upload.js
@@ -188,13 +188,13 @@ export async function onRequestPost(context) {  // Contents of context object
     const returnFormat = url.searchParams.get('returnFormat') || 'default';
     let returnLink = '';
     if (returnFormat === 'full') {
-        returnLink = `${url.origin}/file/${fullId}`;
+        returnLink = `${url.origin}/file/${encodeURIComponent(fullId)}`;
     } else {
-        returnLink = `/file/${fullId}`;
+        returnLink = `/file/${encodeURIComponent(fullId)}`;
     }
 
     // 清除CDN缓存
-    const cdnUrl = `https://${url.hostname}/file/${fullId}`;
+    const cdnUrl = `https://${url.hostname}/file/${encodeURIComponent(fullId)}`;
     await purgeCDNCache(env, cdnUrl, url);
    
 
